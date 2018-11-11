cbc261ac5aeb58c09f4938370b1816de9d4bb91e
aishwarydhare <aishwarydhare@gmail.com>
2018-11-11 13:25:34 +0530

    minor

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ docker push aishwarydhare/helloredis:latest
 docker push aishwarydhare/hellonode:$SHA
 docker push aishwarydhare/helloredis:$SHA
 kubectl apply -f k8s
-kubectl set image deployments/node-server-deployment server=aishwarydhare/hellonode:$SHA
-kubectl set image deployments/redis-server-deployment server=aishwarydhare/hellonode:$SHA
+kubectl set image deployments/node-server-deployment node-server-deployment=aishwarydhare/hellonode:$SHA
+kubectl set image deployments/redis-server-deployment redis-server-deployment=aishwarydhare/hellonode:$SHA
