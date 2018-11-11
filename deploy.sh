a9e386ffa32f9034d1e91445075028fbe6d8a087
aishwarydhare <aishwarydhare@gmail.com>
2018-11-11 13:16:59 +0530

    ready to deploy on travis

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,3 @@ docker push aishwarydhare/helloredis:$SHA
 kubectl apply -f k8s
 kubectl set image deployments/node-server-deployment server=aishwarydhare/hellonode:$SHA
 kubectl set image deployments/redis-server-deployment server=aishwarydhare/hellonode:$SHA
-
-curl https://raw.githubusercontent.com/helm/helm/master/scripts/get > get_helm.sh
-chmod 700 get_helm.sh
-./get_helm.sh
